29aeae0d397889761027fa43d9d96b8e904efa0c
sathyasangari24 <69054083+sathyasangari24@users.noreply.github.com>
2020-08-12 13:45:38 +0530

    Add files via upload

--- README.md
+++ README.md
@@ -1,1 +1,2 @@
-# C23
+# angryBirdsStage1
+Stage 1 Angry Birds
---
+++ sketch.js
@@ -0,0 +1,33 @@
+const Engine = Matter.Engine;
+const World= Matter.World;
+const Bodies = Matter.Bodies;
+
+var engine, world;
+var box1, box2, ground;
+
+
+function setup(){
+    var canvas = createCanvas(1200,400);
+    engine = Engine.create();
+    world = engine.world;
+
+    
+    ground = new Ground(600,height,1200,20)
+
+    box1 = new Box(700,320,70,70);
+    box2 = new Box(760,200,70,70);
+
+
+    
+}
+
+function draw(){
+    background(0);
+    Engine.update(engine);
+    console.log(box2.body.position.x);
+    console.log(box2.body.position.y);
+    console.log(box2.body.angle);
+    box1.display();
+    box2.display();
+    ground.display();
+}
